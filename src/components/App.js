e38f0959e00efd742e36a4b4cdf7b9b7a0cb9cf3
Katrusy86 <katrin.myronova@gmail.com>
2020-05-05 16:57:16 +0300

    Updated state

--- src/components/App.js
+++ src/components/App.js
@@ -10,27 +10,14 @@ const filterContact = (contacts,filter) => {
 
 class App extends Component {
     state = {
-        contacts: [],
+        contacts: [
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
         filter: '',
       }
-
-// ==================localStorage===============
-    componentDidMount(){
-        const persistedContact = localStorage.getItem('contacts');
-
-        if(persistedContact) {
-            const contacts = JSON.parse(persistedContact);
-            this.setState({contacts})
-        }
-    }
-    
-    componentDidUpdate(prevProps,prevState){
-        if(prevState.contacts !== this.state.contacts){
-        localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
-        }
-    }
-
-// ==================================================
 
     addContact = (contact) => {
         const contactToAdd = {
@@ -38,8 +25,9 @@ class App extends Component {
             id: uuid()
         }
         const {name} = contact
+        // console.log(contact);
         const findContact = this.state.contacts.find(((contact) => contact.name === name))
-        // console.log(newContact)
+        // console.log(findContact)
         if(!findContact) {        
         this.setState(prevState => ({
             contacts:[...prevState.contacts, contactToAdd]
